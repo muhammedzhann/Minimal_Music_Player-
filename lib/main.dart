9a6f4f7d3bb20c2784ffbc3d8c578232536b7693
muha <muhammedzansatybaldy37@gmail.com>
2024-05-07 23:05:57 +0500

    music_player

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:first_app/models/playlist_provider.dart';
+import 'package:first_app/themes/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'pages/home_page.dart';
+
+void main() {
+    runApp(
+      MultiProvider(providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => PlaylistProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
+  }
+}
